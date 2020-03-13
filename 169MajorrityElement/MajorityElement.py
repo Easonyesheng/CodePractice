9dd78f259717183a844e91517e860248b9780565
Eason <preachersheng@sina.com>
2020-03-13 10:48:56 +0800

    0313

---
+++ 169MajorrityElement/MajorityElement.py
@@ -0,0 +1,57 @@
+"""
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+from math import floor
+
+class Solution:
+    def majorityElement(self, nums) -> int:
+        elem_nums = set(nums)
+        N = len(nums)
+        N_half = floor(N/2)
+
+        for i in elem_nums:
+            if nums.count(i) > N_half:
+                return i
+
+if __name__ == "__main__":
+    s = Solution()
+    nums  = [2,2,1,1,1,2,2]
+    print(s.majorityElement(nums))
+
+'''
+摩尔投票法：
+
+核心就是对拼消耗。
+
+玩一个诸侯争霸的游戏，假设你方人口超过总人口一半以上，并且能保证每个人口出去干仗都能一对一同归于尽。最后还有人活下来的国家就是胜利。
+
+那就大混战呗，最差所有人都联合起来对付你（对应你每次选择作为计数器的数都是众数），或者其他国家也会相互攻击（会选择其他数作为计数器的数），但是只要你们不要内斗，最后肯定你赢。
+
+最后能剩下的必定是自己人。
+
+
+
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/majority-element/solution/duo-shu-yuan-su-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
