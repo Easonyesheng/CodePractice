fea3d6bd38ae6f2045851f1d5f951065652241ec
Eason <preachersheng@sina.com>
2020-04-05 15:43:14 +0800

    0405

---
+++ 460LFU/LFU.py
@@ -0,0 +1,86 @@
+"""
+设计并实现最不经常使用（LFU）缓存的数据结构。它应该支持以下操作：get 和 put。
+
+get(key) - 如果键存在于缓存中，则获取键的值（总是正数），否则返回 -1。
+put(key, value) - 如果键不存在，请设置或插入值。当缓存达到其容量时，它应该在插入新项目之前，使最不经常使用的项目无效。在此问题中，当存在平局（即两个或更多个键具有相同使用频率）时，最近最少使用的键将被去除。
+
+进阶：
+你是否可以在 O(1) 时间复杂度内执行两项操作？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lfu-cache
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import collections
+
+class Node:
+    '''double linked list
+    '''
+    def __init__(self, key, val, pre=None, nex=None, freq=0):
+        self.pre = pre
+        self.nex = nex
+        self.freq = freq
+        self.val = val
+        self.key = key
+        
+    def insert(self, nex):
+        nex.pre = self
+        nex.nex = self.nex
+        self.nex.pre = nex
+        self.nex = nex
+    
+def create_linked_list():
+    head = Node(0, 0)
+    tail = Node(0, 0)
+    head.nex = tail
+    tail.pre = head
+    return (head, tail)
+
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.minFreq = 0
+        self.freqMap = collections.defaultdict(create_linked_list)
+        self.keyMap = {}
+
+    def delete(self, node):
+        if node.pre:
+            node.pre.nex = node.nex
+            node.nex.pre = node.pre
+            # if this frq is only one
+            if node.pre is self.freqMap[node.freq][0] and node.nex is self.freqMap[node.freq][-1]:
+                self.freqMap.pop(node.freq)
+        return node.key
+        
+    def increase(self, node):
+        node.freq += 1
+        self.delete(node)
+        self.freqMap[node.freq][-1].pre.insert(node)
+        if node.freq == 1:
+            self.minFreq = 1
+        elif self.minFreq == node.freq - 1:
+            head, tail = self.freqMap[node.freq - 1]
+            if head.nex is tail:
+                self.minFreq = node.freq
+
+    def get(self, key: int) -> int:
+        if key in self.keyMap:
+            self.increase(self.keyMap[key])
+            return self.keyMap[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity != 0:
+            if key in self.keyMap:
+                node = self.keyMap[key]
+                node.val = value
+            else:
+                node = Node(key, value)
+                self.keyMap[key] = node
+                self.size += 1
+            if self.size > self.capacity:
+                self.size -= 1
+                deleted = self.delete(self.freqMap[self.minFreq][0].nex)
+                self.keyMap.pop(deleted)
+            self.increase(node)
